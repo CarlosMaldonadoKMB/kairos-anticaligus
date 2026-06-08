5a2b4f9e93f7b19601c7bcb26938f4cdf3e5685f
CarlosMaldonadoKMB <carlos.maldonado@kairosmb.org>
2026-06-07 23:05:15 -0400

    Transformación a PWA con logos corporativos

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,22 @@ export default defineConfig({
       manifest: {
         name: 'Kairos Anticaligus',
         short_name: 'Kairos',
-        theme_color: '#ffffff',
+        theme_color: '#0f172a', 
         background_color: '#ffffff',
         display: 'standalone',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
